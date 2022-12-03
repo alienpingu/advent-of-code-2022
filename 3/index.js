1b52ad21862cea8f07b73f79dd10e58539b89179
sbertocco <samuele.bertocco@soprasteria.com>
2022-12-04 00:42:37 +0100

    [day3] best pratices

--- 3/index.js
+++ 3/index.js
@@ -2,56 +2,57 @@ const fs = require("fs");
 
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-function splitString(string) {
-    let middle = Math.floor(string.length / 2);
-    return([string.substr(0, middle),string.substr(middle)]);
-}
-
-function compareTwoString(string1, string2) {
-    let result = false;
-    string1.split('').forEach(c1 => (string2.includes(c1)) ? result = c1 : null)
-    return (result);
-}
-
-function compareThreeString(string1, string2, string3) {
-    let result = false;
-    string1.split('').forEach(c1 => (string2.includes(c1)) ? (string3.includes(c1)) ? result = c1 : null : null)
-    return (result);
+function compareString(stringArray) {
+  let result = false;
+  stringArray.length === 2
+    ? stringArray[0]
+        .split("")
+        .forEach((c1) => (stringArray[1].includes(c1) ? (result = c1) : null))
+    : stringArray[0]
+        .split("")
+        .forEach((c1) =>
+          stringArray[1].includes(c1)
+            ? stringArray[2].includes(c1)
+              ? (result = c1)
+              : null
+            : null
+        );
+  return result;
 }
 
 function computeValue(char) {
-    let result = 1
-    letters.forEach((e,i) => (e === char) ? result += i : null)
-    return(result)
+  let result = 1;
+  letters.forEach((e, i) => (e === char ? (result += i) : null));
+  return result;
 }
 
 function groupArr(data, n) {
-    var group = [];
-    for (var i = 0, j = 0; i < data.length; i++) {
-        if (i >= n && i % n === 0)
-            j++;
-        group[j] = group[j] || [];
-        group[j].push(data[i])
-    }
-    return group;
-}
-
-function main(input) {
-    let total = 0;
-    input.split('\r\n').forEach((e,i)=> {
-        let char = compareTwoString(splitString(e)[0],splitString(e)[1]);
-        (char !== false) ? total = Number(total) + Number(computeValue(char)) : null
-   })
-   return(total);
-}
-
-function main2(input) {
-    let total = 0;
-    groupArr(input.split('\r\n'), 3).forEach(e => total += computeValue(compareThreeString(e[0],e[1],e[2])));
-    return(total);
+  var group = [];
+  for (var i = 0, j = 0; i < data.length; i++) {
+    if (i >= n && i % n === 0) j++;
+    group[j] = group[j] || [];
+    group[j].push(data[i]);
+  }
+  return group;
 }
 
 fs.readFile("./input.txt", "utf8", (err, data) => {
-    console.log("Answer #1: ",main(data));
-    console.log("Answer #2: ",main2(data));
+    let total = [0,0];
+    // Main First Part
+    data.split("\r\n").forEach((e, i) => {
+      let char = compareString([
+        e.slice(0, e.length / 2),
+        e.slice(e.length / 2),
+      ]);
+      char !== false
+        ? (total[0] = Number(total[0]) + Number(computeValue(char)))
+        : null;
+    });
+    // Main Second Part
+    groupArr(data.split("\r\n"), 3).forEach(
+      (e) => (total[1] += computeValue(compareString([e[0], e[1], e[2]])))
+    );
+    // Print Answers
+    console.log("Answer #1: ", total[0]);
+    console.log("Answer #2: ", total[1]);
 });
